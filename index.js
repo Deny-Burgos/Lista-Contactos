a0dfab7a63b49d9c8fa68b1a7208974297fc4810
Deny <denyburgos1305@gmail.com>
2023-05-25 19:20:44 -0400

    se agregó el evento submit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let contactValidation = false;
 let phoneValidation = false;
 
 const validateInput = (input, isValidedRegex, texShow) => {
-    formBtn.disabled = contactValidation && phoneValidation ? false : true;
+  formBtn.disabled = contactValidation && phoneValidation ? false : true;
   if (input.value === "") {
     input.classList.remove("wrong", "correct");
     texShow.classList.remove("show");
@@ -24,14 +24,27 @@ const validateInput = (input, isValidedRegex, texShow) => {
   }
 };
 
-contactInput.addEventListener("input", e => {
+contactInput.addEventListener("input", (e) => {
   contactValidation = CONTACT_REGEX.test(contactInput.value);
   const texShow = form.children[1];
   validateInput(contactInput, contactValidation, texShow);
 });
 
-phoneInput.addEventListener("input", e => {
+phoneInput.addEventListener("input", (e) => {
   phoneValidation = PHONE_REGEX.test(phoneInput.value);
   const texShow = form.children[3];
   validateInput(phoneInput, phoneValidation, texShow);
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <input class="agendado" type="text" value="${contactInput.value}" readonly>
+    <input class="agendado" type="text" value="${phoneInput.value}" readonly>
+    <button class="delete-btn"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg></button>
+    <button class="edit-btn"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg></button>
+  `;
+  list.append(li);
+  localStorage.setItem("listaContactos", list.innerHTML);
 });
